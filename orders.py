ee6dea51649b27bd533c3b5998098f6144d84210
habimana <rugwiroclement@gmail.com>
2025-01-17 11:41:57 -0800

    added rabbitmq

--- orders.py
+++ orders.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, requests
+import pika
+import json
 
 # Microservice 2: Order Service
 order_service = Flask(__name__)
@@ -8,6 +10,15 @@ orders = {
     1: {"id": 1, "user_id": 1, "item": "Dance Class Subscription", "price": 50},
     2: {"id": 2, "user_id": 2, "item": "Private Dance Lesson", "price": 100}
 }
+
+# RabbitMQ connection setup
+def publish_event(event):
+    print('in mq')
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='user_events')
+    channel.basic_publish(exchange='', routing_key='user_events', body=json.dumps(event))
+    connection.close()
 
 @order_service.route('/orders', methods=['GET'])
 def get_orders():
@@ -27,18 +38,19 @@ def get_orders_by_user(user_id):
         return jsonify({"orders": user_orders}), 200
     return jsonify({"error": "No orders found for this user"}), 404
 
-@order_service.route('/orders/<int:order_id>/user', methods=['GET'])
-def get_order_with_user(order_id):
+@order_service.route('/orders/<int:order_id>/<int:user_id>', methods=['GET'])
+def get_order_with_user(order_id, user_id):
     order = orders.get(order_id)
+    print(f'getting user ${user_id}')
     if not order:
         return jsonify({"error": "Order not found"}), 404
-
-    user_id = order.get("user_id")
     try:
+        print(f'getting info user ${user_id}')
         user_response = requests.get(f'http://localhost:5001/users/{user_id}')
         if user_response.status_code == 200:
             user_data = user_response.json()
             order_with_user = {"order": order, "user": user_data}
+            publish_event({"event": "order_with_user_retrieved", "order_id": order_id, "user": user_data})
             return jsonify(order_with_user), 200
         else:
             return jsonify({"error": "User not found"}), 404
